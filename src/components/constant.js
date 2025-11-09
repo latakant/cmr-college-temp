1460b6f93b54d5d29a1fa686bb28f423fd248e09
latakant <latakantsharm1996@gmail.com>
2025-11-09 13:13:01 +0530

    feat: Add Vercel deployment workflow and update .gitignore for Vercel directory

--- src/components/constant.js
+++ src/components/constant.js
@@ -8,3 +8,4 @@ export const BA_COURSE_LINKS =
   "https://docs.google.com/document/d/1oWx-T4J0ECY0ep9nezAMWQCybQFJ4iba/edit";
 export const MBA_BBA_COURSE_LINKS =
   "https://docs.google.com/document/d/1Ud4iOGH81T06sAWrnuA3Q18XKY0YcRq0c_rU4EkKtwg/";
+//const CMR_github_actions_token = "lBVhawzQxUSGGaTnlJJC0Hbo";
