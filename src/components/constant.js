dd79d962e7623d9350a9c746f8fce654678def5e
latakant <latakantsharm1996@gmail.com>
2025-11-09 19:07:07 +0530

    fix: Standardize constant names for MBA courses and add BBA study materials links

--- src/components/constant.js
+++ src/components/constant.js
@@ -11,7 +11,7 @@ export const MBA_BBA_COURSE_LINKS =
 
 //STUDY MATERIALS MBA
 //sem-I
-export const Managerial_Economics_LINK =
+export const MANAGERIAL_ECONOMICS_LINK =
   "https://drive.google.com/file/d/11v-YKMnGL2MztMEgycN8Wgaf8HHHQWNi/view";
 export const ACCOUNTING_OR_MANAGERS =
   "https://drive.google.com/file/d/1AQIlFrw4QlrXy7NaP09KLMoaENRq6LhB/view";
@@ -19,11 +19,11 @@ export const DYNAMICS_OF_HUMAN_BEHAVIOR =
   "https://drive.google.com/file/d/16oVio0HO_2rR7wjv2BRHdyYVxmzszGqT/view";
 export const INFORMATION_SYSTEM_AND_BUSINESS_ANALYTICS =
   "https://drive.google.com/file/d/1vWK7LKG268aYoOAbdh2BLfjhfkjVce7F/view";
-export const MARKETING_MANAGEMENT =
+export const MARKETING_MANAGEMENT_MBA =
   "https://drive.google.com/file/d/1vWK7LKG268aYoOAbdh2BLfjhfkjVce7F/view";
 export const BUSINESS_STATISTICS_WITH_R =
   "https://drive.google.com/file/d/1Kwounr9KPykIO-kUU-ZgDvLzAVynyvUp/view";
-export const HUMAN_RESOURCE_MANAGEMENT =
+export const HUMAN_RESOURCE_MANAGEMENT_MBA =
   "https://drive.google.com/file/d/1u8H19TnSuWfYZcpDOvjaloXOebC6Cz9e/view";
 
 //sem-II
@@ -31,7 +31,7 @@ export const INTERNATIONAL_BUSINESS_MANAGEMENT =
   "https://drive.google.com/file/d/1zRYMjtZygX68JVANFpKteYQ2uPjA8vxl/view";
 export const BUSINESS_RESEARCH_METHODS =
   "https://drive.google.com/file/d/1wyVaZ1n0sgihQ9Ioh4AOKQUqnHiZXL1k/view";
-export const FINANCIAL_MANAGEMENT =
+export const FINANCIAL_MANAGEMENT_MBA =
   "https://drive.google.com/file/d/1J-u8172mgMUNxMieLiCQ0FI69bLJxu0i/view";
 export const OPERATIONS_RESEARCH_FOR_MANAGERS =
   "https://drive.google.com/file/d/1UdZX_iG2kMRf7CWI7AFbq0HYVWCtGTDz/view?usp=sharing";
@@ -39,7 +39,7 @@ export const OPERATIONS_RESEARCH_FOR_MANAGERS =
 //sem-III
 export const STRATEGIC_ANALYSIS_AND_MANAGEMENT =
   "https://drive.google.com/file/d/1eHsZAncU-eXZY3qxgGQWQf_ZlPh1vBOS/view?usp=sharing";
-export const OPERATIONS_MANAGEMENT =
+export const OPERATIONS_MANAGEMENT_MBA =
   "https://drive.google.com/file/d/1CgikTczkMt5lO0g7UO32Vf1UInYkr_H-/view?usp=sharing";
 
 //sem-IV
@@ -54,3 +54,39 @@ export const TECHNOLOGY_MANAGEMENT =
 
 //STUDY MATERIALS BBA
 //sem-I
+export const FINANCIAL_ACCOUNTING =
+  "https://drive.google.com/file/d/1VQHi_Lig-kOrk3UIUG7AFxxRUGqBT3B0/view?usp=sharing";
+export const BUSINESS_AND_TECHNOLOGY =
+  "https://drive.google.com/file/d/1aj6UDExfrjp7Q5REIR2e_ilTOIXIMvkH/view?usp=sharing";
+export const BUSINESS_ECONOMICS =
+  "https://drive.google.com/file/d/1HdU02TyPvd7_BksuxrtOreesOarZKnwf/view?usp=sharing";
+export const BUSINESS_MATHEMATICS =
+  "https://drive.google.com/file/d/1efl2qQAmXxT9Ld0SvxG4Ru5dudZfDyWd/view?usp=sharing";
+//sem-II
+export const PRINCIPLES_AND_PRACTICES_OF_MANAGEMENT =
+  "https://drive.google.com/file/d/1GqTnwU4evqay9XBH_zI4V73BFxmotVkS/view?usp=sharing";
+export const MACRO_ECONOMICS =
+  "https://drive.google.com/file/d/14N92HmLcdxmB7WBP6nBDTPRT8c09Ibh7/view?usp=sharing";
+export const CORPORATE_BUSINESS_LAW =
+  "https://drive.google.com/file/d/16kIYtfit-4Lg9CnF6iGEzMiZA5ucyTJi/view?usp=sharing";
+export const BUSINESS_STATISTICS_WITH_ADVANCED_EXCEL =
+  "https://drive.google.com/file/d/1uXvy2_BW6Wh3hOZ-SPise_yFmeN_N6F9/view?usp=drive_link";
+//sem-III
+export const CORPORATE_ACCOUNTING =
+  "https://drive.google.com/file/d/14Gy_XbiuCd-j7_TCLza_SliZUiMW51Qe/view?usp=sharing";
+export const HUMAN_RESOURCE_MANAGEMENT_BBA =
+  "https://drive.google.com/file/d/1poFPJdZMDJHTPAYsaE3zRwRhQbw87NBT/view?usp=sharing";
+export const MARKETING_MANAGEMENT_BBA =
+  "https://drive.google.com/file/d/1Ohn0V-mTcBQXVFLCTHI_dZmdP_mJvWG7/view?usp=sharing";
+export const OPERATIONS_MANAGEMENT_BBA =
+  "https://drive.google.com/file/d/12ZvPdnIMXuMBWyTijbnuB4L7YHdCVlbb/view?usp=sharing";
+//sem-IV
+export const FINANCIAL_MANAGEMENT_BBA = "";
+export const BUSINESS_RESEARCH_METHODS_BBA = "";
+export const ORGANISATIONAL_BEHAVIOR = "";
+//sem-V
+export const COST_MANAGEMENT_ACCOUNTING = "";
+export const OPERATIONS_RESEARCH = "";
+export const DIRECT_TAXATION = "";
+export const STRATEGIC_MANAGEMENT = "";
+export const ENTREPRENEURSHIP_DEVELOPMENT = "";
