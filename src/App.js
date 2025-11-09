4c38c0ecc8254717b5ffe62f186cfa3d0631b705
latakant <latakantsharm1996@gmail.com>
2025-11-09 12:46:38 +0530

    fix: Format constants in constant.js for consistency and add newlines

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import "./App.css"; 
+import "./App.css";
 import Home from "./components/Home";
 import AboutCMR from "./components/AboutUs";
 // import CoursesTable from "./components/Course";
 import StudentNotices from "./components/Students";
 // import ContactUs from "./components/Contact/ContactUs";
 import FacultyTable from "./components/Faculty";
-import { CDOE_PDF, CONTACT_LINK, DRIVE_LINK_FOR_ANTI_RAGGING, PDF_FOR_ANTI_RAGGING } from "./components/constant";
+import {
+  CDOE_PDF,
+  CONTACT_LINK,
+  DRIVE_LINK_FOR_ANTI_RAGGING,
+  PDF_FOR_ANTI_RAGGING,
+} from "./components/constant";
 import TemporaryDashboard from "./components/Temporary";
 
 function App() {
@@ -21,15 +26,34 @@ function App() {
           <h1 className="heading">Centre for Distance and Online Education</h1>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/courses" element={<TemporaryDashboard pdfUrl={MBA_BBA_COURSE_LINKS} isDriveLink={true}/>} />
-            <Route path="/student-notices" element={<StudentNotices/>} />
-            <Route path="/faculty-staff" element={<FacultyTable/>} />
-            <Route path="/anti-ragging" element={<TemporaryDashboard pdfUrl={PDF_FOR_ANTI_RAGGING}/>} />
+            <Route
+              path="/courses"
+              element={
+                <TemporaryDashboard
+                  pdfUrl={MBA_BBA_COURSE_LINKS}
+                  isDriveLink={true}
+                />
+              }
+            />
+            <Route path="/student-notices" element={<StudentNotices />} />
+            <Route path="/faculty-staff" element={<FacultyTable />} />
+            <Route
+              path="/anti-ragging"
+              element={<TemporaryDashboard pdfUrl={PDF_FOR_ANTI_RAGGING} />}
+            />
             <Route path="/students" element={<div>Students Page</div>} />
             <Route path="/results" element={<div>Results Page</div>} />
-            <Route path="/contact" element={<TemporaryDashboard pdfUrl={CONTACT_LINK} isDriveLink={true}  />} />
+            <Route
+              path="/contact"
+              element={
+                <TemporaryDashboard pdfUrl={CONTACT_LINK} isDriveLink={true} />
+              }
+            />
             <Route path="/about" element={<AboutCMR />} />
-            <Route path="/brochures" element={<TemporaryDashboard pdfUrl={CDOE_PDF}/>} />
+            <Route
+              path="/brochures"
+              element={<TemporaryDashboard pdfUrl={CDOE_PDF} />}
+            />
           </Routes>
         </div>
       </div>
--- src/components/constant.js
+++ src/components/constant.js
@@ -1,5 +1,10 @@
-export const PDF_FOR_ANTI_RAGGING = "https://www.cmr.edu.in/wp-content/uploads/2025/10/Anti-Ragging-2025.pdf"
-export const CONTACT_LINK = "https://drive.google.com/file/d/1ny19SWgETrUD-FPFYDVUBPz0qNOxz-JX/view?usp=sharing"
-export const CDOE_PDF = "https://www.cmr.edu.in/wp-content/uploads/2025/07/CMRU_SOM_6-Page-Flyer_Digital.pdf"
-export const BA_COURSE_LINKS = "https://docs.google.com/document/d/1oWx-T4J0ECY0ep9nezAMWQCybQFJ4iba/edit"
-export const MBA_BBA_COURSE_LINKS = "https://docs.google.com/document/d/1Ud4iOGH81T06sAWrnuA3Q18XKY0YcRq0c_rU4EkKtwg/"
+export const PDF_FOR_ANTI_RAGGING =
+  "https://www.cmr.edu.in/wp-content/uploads/2025/10/Anti-Ragging-2025.pdf";
+export const CONTACT_LINK =
+  "https://drive.google.com/file/d/1ny19SWgETrUD-FPFYDVUBPz0qNOxz-JX/view?usp=sharing";
+export const CDOE_PDF =
+  "https://www.cmr.edu.in/wp-content/uploads/2025/07/CMRU_SOM_6-Page-Flyer_Digital.pdf";
+export const BA_COURSE_LINKS =
+  "https://docs.google.com/document/d/1oWx-T4J0ECY0ep9nezAMWQCybQFJ4iba/edit";
+export const MBA_BBA_COURSE_LINKS =
+  "https://docs.google.com/document/d/1Ud4iOGH81T06sAWrnuA3Q18XKY0YcRq0c_rU4EkKtwg/";
