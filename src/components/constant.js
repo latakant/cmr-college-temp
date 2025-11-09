b1805fa1b50821ffc45a43f81a713089afdad40d
latakant <latakantsharm1996@gmail.com>
2025-11-09 18:45:31 +0530

    fix: Add study materials links for MBA and BBA courses in constant.js

--- src/components/constant.js
+++ src/components/constant.js
@@ -8,3 +8,49 @@ export const BA_COURSE_LINKS =
   "https://docs.google.com/document/d/1oWx-T4J0ECY0ep9nezAMWQCybQFJ4iba/";
 export const MBA_BBA_COURSE_LINKS =
   "https://docs.google.com/document/d/1Ud4iOGH81T06sAWrnuA3Q18XKY0YcRq0c_rU4EkKtwg/";
+
+//STUDY MATERIALS MBA
+//sem-I
+export const Managerial_Economics_LINK =
+  "https://drive.google.com/file/d/11v-YKMnGL2MztMEgycN8Wgaf8HHHQWNi/view";
+export const ACCOUNTING_OR_MANAGERS =
+  "https://drive.google.com/file/d/1AQIlFrw4QlrXy7NaP09KLMoaENRq6LhB/view";
+export const DYNAMICS_OF_HUMAN_BEHAVIOR =
+  "https://drive.google.com/file/d/16oVio0HO_2rR7wjv2BRHdyYVxmzszGqT/view";
+export const INFORMATION_SYSTEM_AND_BUSINESS_ANALYTICS =
+  "https://drive.google.com/file/d/1vWK7LKG268aYoOAbdh2BLfjhfkjVce7F/view";
+export const MARKETING_MANAGEMENT =
+  "https://drive.google.com/file/d/1vWK7LKG268aYoOAbdh2BLfjhfkjVce7F/view";
+export const BUSINESS_STATISTICS_WITH_R =
+  "https://drive.google.com/file/d/1Kwounr9KPykIO-kUU-ZgDvLzAVynyvUp/view";
+export const HUMAN_RESOURCE_MANAGEMENT =
+  "https://drive.google.com/file/d/1u8H19TnSuWfYZcpDOvjaloXOebC6Cz9e/view";
+
+//sem-II
+export const INTERNATIONAL_BUSINESS_MANAGEMENT =
+  "https://drive.google.com/file/d/1zRYMjtZygX68JVANFpKteYQ2uPjA8vxl/view";
+export const BUSINESS_RESEARCH_METHODS =
+  "https://drive.google.com/file/d/1wyVaZ1n0sgihQ9Ioh4AOKQUqnHiZXL1k/view";
+export const FINANCIAL_MANAGEMENT =
+  "https://drive.google.com/file/d/1J-u8172mgMUNxMieLiCQ0FI69bLJxu0i/view";
+export const OPERATIONS_RESEARCH_FOR_MANAGERS =
+  "https://drive.google.com/file/d/1UdZX_iG2kMRf7CWI7AFbq0HYVWCtGTDz/view?usp=sharing";
+
+//sem-III
+export const STRATEGIC_ANALYSIS_AND_MANAGEMENT =
+  "https://drive.google.com/file/d/1eHsZAncU-eXZY3qxgGQWQf_ZlPh1vBOS/view?usp=sharing";
+export const OPERATIONS_MANAGEMENT =
+  "https://drive.google.com/file/d/1CgikTczkMt5lO0g7UO32Vf1UInYkr_H-/view?usp=sharing";
+
+//sem-IV
+export const LEGAL_ENVIRONMENT_OF_BUSINESS =
+  "https://drive.google.com/file/d/1JpL86SRXtp6Nk6g9rPfdP7nsYfGtsaaR/view?usp=sharing";
+export const LEADERSHIP_AND_CAPACITY_BUILDING =
+  "https://drive.google.com/file/d/1x9QOBSSxQ3UHnACL2fxlF0HJkPn5wEeJ/view?usp=sharing";
+export const TOTAL_QUALITY_MANAGEMENT =
+  "https://drive.google.com/file/d/1pMO21d5RQ0KkNkGEbqozc5UtmGxYKFO_/view?usp=sharing";
+export const TECHNOLOGY_MANAGEMENT =
+  "https://drive.google.com/file/d/1BjuBmNxwqPLrh_RuSGb-7eILp94-4-Yq/view?usp=sharing";
+
+//STUDY MATERIALS BBA
+//sem-I
