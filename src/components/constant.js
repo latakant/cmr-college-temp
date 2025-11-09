23ca6f15ab88b0de6c8f8e85e08159782edb6d48
latakant <latakantsharm1996@gmail.com>
2025-11-09 14:28:34 +0530

    fix: Update BA_COURSE_LINKS URL to remove edit parameter for correct access

--- src/components/constant.js
+++ src/components/constant.js
@@ -5,6 +5,6 @@ export const CONTACT_LINK =
 export const CDOE_PDF =
   "https://www.cmr.edu.in/wp-content/uploads/2025/07/CMRU_SOM_6-Page-Flyer_Digital.pdf";
 export const BA_COURSE_LINKS =
-  "https://docs.google.com/document/d/1oWx-T4J0ECY0ep9nezAMWQCybQFJ4iba/edit";
+  "https://docs.google.com/document/d/1oWx-T4J0ECY0ep9nezAMWQCybQFJ4iba/";
 export const MBA_BBA_COURSE_LINKS =
   "https://docs.google.com/document/d/1Ud4iOGH81T06sAWrnuA3Q18XKY0YcRq0c_rU4EkKtwg/";
